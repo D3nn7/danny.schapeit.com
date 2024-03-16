27fb87858af335e7d31b8ed3b3cddbacda53cccf
Danny Schapeit <dannyschapeit@slynite.com>
2024-03-16 18:03:58 +0100

    Add new design
    - add new headlines as images
    - add new logo as image
    - add new test background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,74 @@
-import EasterEgg from '@/components/easterEgg'
-import SocialMedia from '@/components/socialMediaLinks'
-import { ChevronRightIcon } from '@heroicons/react/20/solid'
-import Link from 'next/link'
+import EasterEgg from "@/components/easterEgg";
+import SocialMedia from "@/components/socialMediaLinks";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center space-y-8'>
-		<EasterEgg />
-		<div className='justify-center text-center md:space-x-4'>
-			<h3 className='font-high-summit text-4xl text-silver'>About</h3>
-			<p className='text-left md:px-36'>
-				I&apos;m Danny Schapeit (or D3nn7) a developer from Germany with a preference for open source and privacy protected software.
-				Most of all I like to develop projects for my company <Link href={'https://slynite.com'} className='underline'>Slynite</Link>.
-				My skills and knowledge can be found in my <Link href={'/cv'} className='underline'>CV</Link>. I mainly work on my Macbook, which is why I have advanced Unix knowledge.
-				In my free time I like to explore the world, play games or steam sometimes on <Link href={'https://www.twitch.tv/dannyschapeit'} className='underline'>Twitch</Link>. I am also an absolute cat lover and I like to play table tennis. Video/Photo editing is also one of my hobbies (I have good knowledge of some Adobe CC products).
-			</p>
+    <main className="flex flex-col items-center space-y-8">
+      <EasterEgg />
+      <div className="justify-center text-center md:space-x-4">
+        <Image
+          className="mx-auto"
+          src="/text/who-white.png"
+          width={100}
+          height={20}
+          alt="Who"
+        />
+        <p className="text-left md:px-36">
+          I&apos;m Danny Schapeit (or D3nn7) a developer from Germany with a
+          preference for open source and privacy protected software. Most of all
+          I like to develop projects for my company{" "}
+          <Link href={"https://slynite.com"} className="underline">
+            Slynite
+          </Link>
+          . My skills and knowledge can be found in my{" "}
+          <Link href={"/cv"} className="underline">
+            CV
+          </Link>
+          . I mainly work on my Macbook, which is why I have advanced Unix
+          knowledge. In my free time I like to explore the world, play games or
+          steam sometimes on{" "}
+          <Link
+            href={"https://www.twitch.tv/dannyschapeit"}
+            className="underline"
+          >
+            Twitch
+          </Link>
+          . I am also an absolute cat lover and I like to play table tennis.
+          Video/Photo editing is also one of my hobbies (I have good knowledge
+          of some Adobe CC products).
+        </p>
 
-			<div className='md:space-x-6 space-y-2 md:space-y-0 md:flex justify-center mt-4 md:m-6'>
-				<Link href={'/cv'} className='flex hover:underline'>CV <ChevronRightIcon className='w-6 h-6' /> </Link>
-				<Link href={'mailto:danny@schapeit.com'} className='flex hover:underline'>Drop a mail <ChevronRightIcon className='w-6 h-6' /></Link>
-				<Link href={'https://dev.to/d3nn7'} target='_blank' className='flex hover:underline'>Blog (Dev.to) <ChevronRightIcon className='w-6 h-6' /></Link>
-				<Link href={'https://bsky.app/profile/schapeit.com'} target='_blank' className='flex hover:underline'>Bluesky <ChevronRightIcon className='w-6 h-6' /></Link>
-			</div>
-		</div>
+        <div className="md:space-x-6 space-y-2 md:space-y-0 md:flex justify-center mt-4 md:m-6">
+          <Link href={"/cv"} className="flex hover:underline">
+            CV <ChevronRightIcon className="w-6 h-6" />{" "}
+          </Link>
+          <Link
+            href={"mailto:danny@schapeit.com"}
+            className="flex hover:underline"
+          >
+            Drop a mail <ChevronRightIcon className="w-6 h-6" />
+          </Link>
+          <Link
+            href={"https://dev.to/d3nn7"}
+            target="_blank"
+            className="flex hover:underline"
+          >
+            Blog (Dev.to) <ChevronRightIcon className="w-6 h-6" />
+          </Link>
+          <Link
+            href={"https://bsky.app/profile/schapeit.com"}
+            target="_blank"
+            className="flex hover:underline"
+          >
+            Bluesky <ChevronRightIcon className="w-6 h-6" />
+          </Link>
+        </div>
+      </div>
 
-		<SocialMedia />
+      <SocialMedia />
     </main>
-  )
+  );
 }
--- components/socialMediaLinks.tsx
+++ components/socialMediaLinks.tsx
@@ -1,17 +1,48 @@
+import Image from "next/image";
 import SocialLink from "./socialLink";
 
 export default function SocialMedia() {
-    return(
-        <div className='justify-center text-center space-x-4 pb-12'>
-            <h3 className='font-high-summit text-4xl text-silver'>Socials</h3>
-            <div className='flex'>
-                <SocialLink name={'Github'} link={'https://github.com/d3nn7'} image={'/icons/github-512.png'} />
-                <SocialLink name={'Instagram'} link={'https://instagram.com/d3nn7.me'} image={'/icons/instagram-512.png'} />
-                <SocialLink name={'Twitch'} link={'https://www.twitch.tv/dannyschapeit'} image={'/icons/twitch-512.png'} />
-                <SocialLink name={'Dev.to'} link={'https://dev.to/d3nn7'} image={'/icons/dev.to-512.png'} />
-                <SocialLink name={'LinkedIn'} link={'https://www.linkedin.com/in/danny-schapeit-a23b5a232'} image={'/icons/linkedin-512.png'} />
-                <SocialLink name={'Xing'} link={'https://www.xing.com/profile/DannyTobias_Schapeit'} image={'/icons/xing-512.png'} />
-            </div>
-        </div>
-    )
+  return (
+    <div className="justify-center text-center space-x-4 pb-12">
+      <Image
+        className="mx-auto"
+        src="/text/socials-white.png"
+        width={300}
+        height={70}
+        alt="Who"
+      />
+      <div className="flex">
+        <SocialLink
+          name={"Github"}
+          link={"https://github.com/d3nn7"}
+          image={"/icons/github-512.png"}
+        />
+        <SocialLink
+          name={"Instagram"}
+          link={"https://instagram.com/d3nn7.me"}
+          image={"/icons/instagram-512.png"}
+        />
+        <SocialLink
+          name={"Twitch"}
+          link={"https://www.twitch.tv/dannyschapeit"}
+          image={"/icons/twitch-512.png"}
+        />
+        <SocialLink
+          name={"Dev.to"}
+          link={"https://dev.to/d3nn7"}
+          image={"/icons/dev.to-512.png"}
+        />
+        <SocialLink
+          name={"LinkedIn"}
+          link={"https://www.linkedin.com/in/danny-schapeit-a23b5a232"}
+          image={"/icons/linkedin-512.png"}
+        />
+        <SocialLink
+          name={"Xing"}
+          link={"https://www.xing.com/profile/DannyTobias_Schapeit"}
+          image={"/icons/xing-512.png"}
+        />
+      </div>
+    </div>
+  );
 }
