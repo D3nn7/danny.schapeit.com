af27e079f87a14dcc08edf2105de6cdc137a9898
Danny Schapeit <dannyschapeit@slynite.com>
2026-04-14 07:30:09 +0000

    fixed mainpage

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,22 @@ export default function Home() {
       <EasterEgg />
       <div className="justify-center text-center md:space-x-4">
         <h3 className="font-high-summit text-4xl text-silver">About me</h3>
-        <p className="text-left md:px-36">
-          Hallo, ich bin Danny.
-          Ich bin Softwareentwickler mit Schwerpunkt auf Android und Fullstack und arbeite aktuell an produktiven Anwendungen im Unternehmensumfeld. Dabei entwickle ich sowohl neue Features als auch bestehende Systeme weiter und beschäftige mich zusätzlich mit Themen wie Architektur, Deployment und Systemintegration.
-          Mein Fokus liegt darauf, Software sauber, nachvollziehbar und langfristig wartbar zu entwickeln. Besonders interessieren mich dabei Themen wie Privacy, Security und moderne Technologien rund um mobile Anwendungen und Backend-Systeme.
-          Neben der Arbeit an bestehenden Produkten beschäftige ich mich auch mit eigenen Projekten, unter anderem im Open-Source-Bereich. Auch wenn nicht jedes Projekt weitergeführt wird, ist für mich der Lernprozess und das Verständnis für Systeme genauso wichtig wie das fertige Ergebnis.
-          Auf{" "}
+        <div className="text-left md:px-36 whitespace-pre-line">
+          <p>Hallo, ich bin Danny.</p>
+          <p>Ich bin Softwareentwickler mit Schwerpunkt auf Android und Fullstack und arbeite aktuell an produktiven Anwendungen im Unternehmensumfeld.</p>
+          <br/>
+          <p>Dabei entwickle ich sowohl neue Features als auch bestehende Systeme weiter und beschäftige mich zusätzlich mit Themen wie Architektur, Deployment und Systemintegration.</p>
+          <br/>
+          <p>Mein Fokus liegt darauf, Software sauber, nachvollziehbar und langfristig wartbar zu entwickeln. Besonders interessieren mich dabei Themen wie Privacy, Security und moderne Technologien rund um mobile Anwendungen und Backend-Systeme.</p>
+          <br/>
+          <p>Neben der Arbeit an bestehenden Produkten beschäftige ich mich auch mit eigenen Projekten, unter anderem im Open-Source-Bereich. Auch wenn nicht jedes Projekt weitergeführt wird, ist für mich der Lernprozess und das Verständnis für Systeme genauso wichtig wie das fertige Ergebnis.</p>
+          <br/>
+          <p>Auf{" "}
           <Link href={"/cv"} className="underline">
             dieser Seite
           </Link>
-          findest du einen Überblick über meine Projekte und meine Arbeit.
-        </p>
+          {" "}findest du einen Überblick über meine Projekte und meine Arbeit.</p>
+        </div>
 
         <div className="md:space-x-6 space-y-2 md:space-y-0 md:flex justify-center mt-4 md:m-6">
           <Link href={"/cv"} className="flex hover:underline">
